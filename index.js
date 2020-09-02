e0bcfcb1a0f699f28fa065b1fd5c060c344d09be
Aeson Johnson <aeson.m.johnson@gmail.com>
2020-09-01 20:04:00 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
 const express = require("express");
-
+const shortid = require("shortid");
 const server = express();
+
+let hubs = [];
 
 server.get("/", (req, res) => {
     res.send("hey there");
+})
+
+server.get("/hello", (req, res) => {
+    res.json({ hello: "lambda school" })
+})
+
+// CREATE {POST}
+server.post("/api/hubs", (req, res) => {
+    const hubInfo = req.body; 
+
+    hubInfo.id = shortid.generate;
+
+    hubs.push(hubInfo);
+    res.status(201).json(hubInfo);
 })
 
 const PORT = 5000;
